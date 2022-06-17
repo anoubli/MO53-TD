4c5d059d6e8a9e34fa8468427f88b268bfd7d5f8
Antoine RICHARD <antoine.richard68@gmail.com>
2022-06-17 21:33:21 +0200

    add process logic

--- process.cpp
+++ process.cpp
@@ -1,7 +1,8 @@
 #include "process.h"
 
-void process::execute()
+void process::execute(std::string path_to_file)
 {
-
-
+    *_storage = _loader->load(path_to_file);
+    _calc->calculation(_storage);
+    _writer->write(_storage);
 }
