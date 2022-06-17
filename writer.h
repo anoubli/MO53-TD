001c5d32f317b9b39ac6f93934741bd208aaa317
Antoine RICHARD <antoine.richard68@gmail.com>
2022-06-17 23:00:37 +0200

    fix error doxygen comment writer classes

--- writer.h
+++ writer.h
@@ -1,14 +1,36 @@
+/**
+ *
+ * @file writer.h
+ * @author antoine richard
+ * @date 2022-06-10
+ * @brief Contains writer classes
+ * 
+ */
+
+//
+// Define guards
 #ifndef WRITER_H
 #define WRITER_H
 
-
+/**
+ * @brief Abstract class (pure virtual) that define the behavior of a writer class
+ * 
+ */
 class writer { // pure virtual
-}; // any output (file - text or bin, screen) inherit from this one
+};
 
+/**
+ * @brief Class used to write data into text file
+ * 
+ */
 class text_writer : public writer {
 
 };
 
+/**
+ * @brief  Class used to write data into text file
+ * 
+ */
 class bin_writer : public writer{
 
 };
